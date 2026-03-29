1bafc5740debaa61a8aa87ccfb7b0253f6feedd8
astra321123 <126397642+astra321123@users.noreply.github.com>
2026-03-29 14:33:42 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,16 +1,20 @@
 document.getElementById("claimBtn").onclick = async () => {
-
     const wallet = document.getElementById("wallet").value;
     const token = document.querySelector("[name='cf-turnstile-response']").value;
 
-    if (!wallet) return alert("Enter wallet");
+    if (!wallet) return alert("Enter FaucetPay email");
 
-    const res = await fetch("/claim", {
-        method:"POST",
-        headers:{"Content-Type":"application/json"},
-        body: JSON.stringify({wallet, token})
-    });
+    try {
+        const res = await fetch("/claim", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ wallet, token })
+        });
 
-    const data = await res.json();
-    document.getElementById("msg").innerText = data.message;
+        const data = await res.json();
+        document.getElementById("msg").innerText = data.message;
+    } catch (err) {
+        document.getElementById("msg").innerText = "Server error, try again later.";
+        console.log(err);
+    }
 };
